4a915df41f6bd5efbdb0ac4794fe73e3cca9e8a1
Marcel Sauer <msauer@Marcels-Air.fritz.box>
2022-11-30 15:57:42 +0100

    t

--- src/main/java/A.java
+++ src/main/java/A.java
@@ -1,4 +1,6 @@
-class A {
+enum Answer { YES, NO, MAYBE }
+
+public class A {
 
 public static final String GOOGLE_APPLICATION_CREDENTIALS="sdfksdfkjlkj3948dfkjsdkfj39420394elfkjsdlkfj";
 public static final String client_secret="d-FL95Q19q7MQmFpd7hHD0Ty";
@@ -6,4 +8,16 @@ public static final String client_secret="d-FL95Q19q7MQmFpd7hHD0Ty";
 	void myJavaFun(String s) {
         boolean b = s.equals("");
     }
+
+Answer interpret(Answer answer) {
+		switch (answer) {
+			case MAYBE:
+				return Answer.YES;
+			case NO:
+				return Answer.MAYBE;
+			// Missing case for 'YES'
+		}
+		throw new RuntimeException("uncaught case: " + answer);
+	}
+
 }
