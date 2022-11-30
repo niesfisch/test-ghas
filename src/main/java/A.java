a8e8bf46e02a8513b2b220209ea2a62ae09d310e
Marcel Sauer <msauer@Marcels-MacBook-Air.local>
2022-11-30 15:53:30 +0100

    t

--- src/main/java/A.java
+++ src/main/java/A.java
@@ -2,4 +2,8 @@ class A {
 
 public static final String GOOGLE_APPLICATION_CREDENTIALS="sdfksdfkjlkj3948dfkjsdkfj39420394elfkjsdlkfj";
 public static final String client_secret="d-FL95Q19q7MQmFpd7hHD0Ty";
+
+	void myJavaFun(String s) {
+        boolean b = s.equals("");
+    }
 }
