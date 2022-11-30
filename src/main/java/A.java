b77fcea820f24e8b9316774745dabe7b0c377b9f
Marcel Sauer <msauer@Marcels-Air.fritz.box>
2022-11-30 11:57:52 +0100

    dependabot

--- src/main/java/A.java
+++ src/main/java/A.java
@@ -1,4 +1,4 @@
 class A {
 
-public static final String GOOGLE_APPLICATION_CREDENTIALS="sdfksdfkjlkj3948dfkjsdkfj39420394elfkjsdlkfj"
+public static final String GOOGLE_APPLICATION_CREDENTIALS="sdfksdfkjlkj3948dfkjsdkfj39420394elfkjsdlkfj";
 }
